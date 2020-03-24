b064c137317afa377dac7f05b3c8538eb10e2867
minhbiken14@gmail.com <minhbiken14@gmail.com>
2020-03-24 22:17:41 +0700

    docker mongodb

---
+++ scripts/mongoseed.sh
@@ -0,0 +1,1 @@
+mongoimport --host mongo-express --port 8081 --username root --password root --authenticationDatabase admin --db NoSQL --collection delete_me --type json --file data.json --jsonArray
