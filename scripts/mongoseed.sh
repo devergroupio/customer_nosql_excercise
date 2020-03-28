7428c16888612e383d66578ada37a7197886d7a0
minhbiken14@gmail.com <minhbiken14@gmail.com>
2020-03-28 22:03:50 +0700

    fixed merge conflic

--- scripts/mongoseed.sh
+++ scripts/mongoseed.sh
@@ -1,1 +1,1 @@
-mongoimport --host mongo --port 27017 --username root --password root --authenticationDatabase=admin -d woption -c eurusd --type csv --file scripts/data.csv --headerline
+gimongoimport --host mongo --port 27017 --username root --password root --authenticationDatabase=admin -d woption -c eurusd --type csv --file scripts/data.csv --headerline
